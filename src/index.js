0b1204ff272c2cd92aac3fae1795a07bfdef7b34
KrysDev <webdevkrys@gmail.com>
2022-02-20 19:18:52 +0000

    rtr

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import App2 from './App2';
 
+import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// import App2 from './App2';
+// ReactDOM.render(<App2 />, document.getElementById('root'));
