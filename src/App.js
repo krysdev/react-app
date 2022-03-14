125a4124cd65cf426182e861e5f55956bf08538c
KrysDev <webdevkrys@gmail.com>
2022-03-14 19:07:50 +0000

    rtr

--- src/App.js
+++ src/App.js
@@ -2,24 +2,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import initialStories from './list';   // default export name: list
 
-// const stories = [
-//   {
-//   title: "React",
-//   url: "https://reactjs.org/",
-//   author: "Jordan Walker",
-//   num_comments: 3,
-//   points: 4,
-//   objectID: 0
-//   },
-//   {
-//   title: "Redux",
-//   url: "https://redux.js.org/",
-//   author: "Dan Abramov, Andrew Clark",
-//   num_comments: 2,
-//   points: 5,
-//   objectID: 1
-//   }
-// ]
 
 function useSemiPersistentState(key, initialState) {
 
